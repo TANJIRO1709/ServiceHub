934fa9349c1eb543ec5b3743255216e3de709ddb
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-10-05 15:40:40 +0530

    only frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Features from './components/Features'; 
 import CTA from './components/CTA';
-import Footer from './components/Footer';
+
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
         <Hero />
         <Features />
         <CTA />
-        <Footer />
+
       </main>
   
   );
