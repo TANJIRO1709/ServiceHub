937f673b9454c4b098c2647316b081371d679b1e
TANJIRO1709 <sujalnitrkl0596@gmail.com>
2024-10-07 12:48:25 +0530

    googleMaps added

--- src/app/Request/page.tsx
+++ src/app/Request/page.tsx
@@ -1,49 +1,60 @@
-"use client"
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api'; 
+"use client";
+import React, { useCallback, useState } from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 
-function page() {
-    const containerStyle = {
-  width: '100%',
-  height: '90vh'
-};
+function MapPage() {  // Renamed 'page' to 'MapPage'
+  const containerStyle = {
+    width: "100%",
+    height: "90vh",
+  };
 
-const center = {
-  lat: 22.253385,
-  lng: 84.906261
-};
-   const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: process.env.NEXT_PUBLIC__GOOGLE_MAPS_API_KEY || '',
-  })
+  const center = {
+    lat: 22.253385,
+    lng: 84.906261,
+  };
 
-  const [map, setMap] = React.useState<google.maps.Map | null>(null)
+  // Loading the Google Maps API
+  const { isLoaded, loadError } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || "",
+  });
 
-  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
-    // This is just an example of getting and using the map instance!!! don't just blindly copy!
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
+  // State to store the map instance
+  const [map, setMap] = useState<google.maps.Map | null>(null);
 
-    setMap(map)
-  }, [])
+  // Callback for when the map is loaded
+  const onLoad = useCallback(
+    (map: google.maps.Map) => {
+      const bounds = new window.google.maps.LatLngBounds(center);
+      map.fitBounds(bounds);
+      setMap(map);
+    },
+    [center]  // Added 'center' to the dependency array
+  );
 
-const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
-    setMap(null)
-}, [])
+  // Callback for when the map is unmounted
+  const onUnmount = useCallback(() => {
+    setMap(null);
+  }, []);
+
+  if (loadError) {
+    return <div>Error loading Google Maps</div>;
+  }
 
   return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={8}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={8}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      { /* You can add markers or other components here */ }
+      <></>
+    </GoogleMap>
+  ) : (
+    <div>Loading...</div>
+  );
 }
 
-export default page
-
+export default MapPage;  // Updated the export to match the component name
